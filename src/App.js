ff2a53d24111a3c71ce1ad3437497bbb560a4e95
elichka <ed44362072@edu.mon.bg>
2021-03-22 19:51:49 +0200

    sus sudeistvieto na moq fr @lubi222

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Presentation1 from './components/Presentation1';
+import Presentation2 from './components/Presentation2';
+import Presentation3 from './components/Presentation3';
+import Presentation4 from './components/Presentation4';
+import Presentation5 from './components/Presentation5';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Presentation1></Presentation1>
+				<Presentation2></Presentation2>
+				<Presentation3></Presentation3>
+				<Presentation4></Presentation4>
+				<Presentation5></Presentation5>
+			</header>
+		</div>
+	);
 }
 
 export default App;
